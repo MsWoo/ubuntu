ec7b22bb700c27ab59978a5da87358e8d8dd12df
MsWoo <minsoo010@naver.com>
2021-12-17 16:25:35 +0900

    add auth, api, storage

---
+++ FullStack/resizable-album/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,34 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "resizablealbum4bf4e47e": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "resizablealbum": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "function": {
+        "S3Triggere037cb73": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "resizablealbums3": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
